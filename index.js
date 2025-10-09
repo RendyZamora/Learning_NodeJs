7d8a19ccc2dae0a0a2376cdce746c5a702ec43ee
RendyZamora <zamorarendi2@gmail.com>
2025-10-09 22:14:43 +0700

    Memasukan beberapa file js

---
+++ index.js
@@ -0,0 +1,22 @@
+ const readline = require("readline")
+const fs = require('fs')
+const rl = readline.createInterface(
+    {
+        input: process.stdin,
+        output: process.stdout
+    }
+)
+
+rl.question("masukan nama anda :", (nama) =>{
+    rl.question("masukan noHp anda :", (noHp) =>{
+        const contact = {nama, noHp}
+        const file = fs.readFileSync('data/contacts.json', "utf8");
+        const contacts = JSON.parse(file);
+        contacts.push(contact)
+        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts))
+
+        rl.close()
+    })
+})
+
+fs.writeFileSync('data/contacts.json','["Heloo World!!"]');

737b0329cdb02861153525f5ba0ef7a913727672
RendyZamora <zamorarendi2@gmail.com>
2025-10-12 16:22:33 +0700

    merubah isi file dan meperbagus kode pada contacts.js dan app.js

--- app.js
+++ app.js
@@ -1,13 +1,72 @@
-const contacts = require('./contacts')
+const yargs = require("yargs")
+const { simpanContact, listContact, detailContact, hapusContact } = require("./contacts")
 
-const main = async ()=>{
-    const nama= await contacts.pertanyaan("Masukan nama anda :");
-    const noHp= await contacts.pertanyaan("Masukan no Hp :");
-    const email= await contacts.pertanyaan("Masukan email anda :");
+yargs.command({
+    command : 'add',
+    describe: 'menambahkan kontak baru',
+    builder: {
+        nama:{
+            describe: 'nama lengkap',
+            demandOption: true,
+            type : 'string'
+        },
+        email:{
+            describe: 'alamat email',
+            demandOption: true,
+            type : 'string'
+        },
+        noHp:{
+            describe: 'nomor HP',
+            demandOption: true,
+            type : 'string'
+        },
+    },
+    handler(inpt){
+        simpanContact(inpt.nama,inpt.email, inpt.noHp);
+    }
+}).demandCommand();
 
+yargs.command(
+    {
+        command : 'list',
+        describe: 'menampilkan daftar identitas',
+        handler(){
+            listContact()
+        }
+    }
+)
 
-    contacts.simpanContact(nama, email , noHp)
-}
+yargs.command(
+    {
+    command : 'detail',
+    describe: 'menampilkan rincian identitas berdasarkan nama',
+    builder: {
+        nama:{
+            describe: 'nama lengkap',
+            demandOption: true,
+            type : 'string'
+        }
+    },
+    handler(arg){
+        detailContact(arg.nama);
+    }
+    }
+)
+yargs.command(
+    {
+    command : 'delete',
+    describe: 'menghapus data identitas berdasarkan nama',
+    builder: {
+        nama:{
+            describe: 'nama lengkap',
+            demandOption: true,
+            type : 'string'
+        }
+    },
+    handler(arg){
+        hapusContact(arg.nama);
+    }
+    }
+)
 
-main()
-
+yargs.parse()
--- contacts.js
+++ contacts.js
@@ -1,34 +1,70 @@
 const readline = require("readline")
 const fs = require('fs')
 
-const rl = readline.createInterface(
-    {
-        input: process.stdin,
-        output: process.stdout
-    }
-)
+// const rl = readline.createInterface(
+//     {
+//         input: process.stdin,
+//         output: process.stdout
+//     }
+// )
 
 const pathdir = './data';
 if (!fs.existsSync(pathdir)){
     fs.mkdirSync(pathdir)
 }
-const pertanyaan = (pesan) =>{
-    return new Promise((resolve, rejects)=>{
-        rl.question( pesan, (nama) =>{
-            resolve(nama)
-        })
-    })
-}
-const simpanContact = (nama, email, noHp)=>{
-    const contact = {nama, email, noHp}
+
+const loadContact = ()=>{
     const file = fs.readFileSync('data/contacts.json', "utf8");
     const contacts = JSON.parse(file);
+    return contacts
+}
+
+const simpanContact = (nama, email, noHp)=>{
+    const contact = {nama, email, noHp}
+    const contacts = loadContact()
+
     contacts.push(contact)
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts))
 
-    rl.close()
+    console.log(`Data ${nama} berhasil ditambahkan`)
+
+    // rl.close()
 }
 
+const listContact = ()=>{
+    const contacts = loadContact();
+    console.log("Daftar Identitas :");
+    contacts.forEach((contact,i) => {
+        console.log(`${i+1}. ${contact.nama}--${contact.noHp}`)
+    });
+}
+
+const detailContact = (nama) =>{
+    const contacts= loadContact();
+    const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+    if(!contact){
+        console.log(`${nama} tidak ada didalam daftar`);
+        return false
+    } 
+   console.log(`nama : ${contact.nama}, email : ${contact.email}, noHp : ${contact.noHp}`)
+}
+
+const hapusContact = (nama)=>{
+    const contacts = loadContact();
+    const newContact = contacts.filter((contact)=>
+        {contact.nama.toLowerCase() !== nama.toLowerCase()}
+);
+    if(contacts.length === newContact.length){
+        console.log(`Data ${nama} tidak ditemukan`)
+        return false;
+    }
+    console.log(newContact)
+    fs.writeFileSync('data/contacts.json',JSON.stringify(newContact));
+    console.log(`Data ${nama} berhasil dihapus`)
+}
+
+
+
 module.exports = {
-    simpanContact,pertanyaan
+    simpanContact, listContact, detailContact, hapusContact
 }
